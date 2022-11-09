28c33a1ee4a7eb970dc834f167c38c8c7f114647
Jehan <jehan.alruwaili@gmail.com>
2022-11-09 03:03:42 +0300

    add card file

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -4,6 +4,7 @@ import Login from './Login';
 import Home from './Home';
 import About from './About';
 import Card from './Card';
+import Create from './Create';
 // import Nav from './Nav';
 
 function Routers() {
@@ -16,6 +17,7 @@ function Routers() {
               <Route path = "/Login" element = {<Login/>}/>
               <Route path = "/About" element = {<About/>}/>
               <Route path = "/Card" element = {<Card/>}/>
+              <Route path="/Create" element = {<Create/>}/>
           </Routes>
         </Router>
     </div>
